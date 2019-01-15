378140e12d611a510c905c3eef0526355bfca8b3
Runtime4642 <46149899+Runtime4642@users.noreply.github.com>
2019-01-15 18:45:28 +0900

    Add files via upload

--- 002.sql
+++ 002.sql
@@ -9,14 +9,19 @@ select date_format(max(hire_date),'%Y년 %m월 %d일') from employees;
 select date_format(min(hire_date),'%Y년 %m월 %d일') from employees;
 
 -- 문제4
-select * from salaries;
-select max(금액) as 최고,min(금액) 최저 from(select max(from_date) as 날짜,max(salary) as 금액 from salaries group by emp_no) a;
 
--- 문제5 88958
-select max(max) as 최고연봉,min(min) as 최저연봉 from (select max(salary) as max,min(salary) as min from salaries group by emp_no) a;
--- 문제6
+select avg(salary) as 평균연봉 from salaries;	 
+
+-- 문제5 
+select max(salary) as 최고연봉,min(salary) as 최소연봉  from salaries;	 
+
+
+-- 문제6 period 방식
 select floor(period_diff(date_format(curdate(),'%Y%m'),date_format(max(birth_date),'%Y%m'))/12) 
  as '최고 어린 사원의 나이' 
 ,floor(period_diff(date_format(curdate(),'%Y%m'),date_format(min(birth_date),'%Y%m'))/12) 
 as'최 연장자의 나이' from employees ;
+
+-- 문제6 그냥 년으로만 가지고 뺌.
+select date_format(curdate(),'%Y')-date_format(max(birth_date),'%Y') as '최고 어린 사원의 나이' ,date_format(curdate(),'%Y')-date_format(min(birth_date),'%Y') as '최 연장자의 나이'from employees;
 
